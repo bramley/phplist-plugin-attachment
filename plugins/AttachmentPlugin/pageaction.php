d962fecb307327ad99a66e845d4ec9b569916409
Duncan Cameron <phplist@dcameron.me.uk>
2017-03-04 17:40:25 +0000

    Add pageaction page to be used for phplist help.

---
+++ plugins/AttachmentPlugin/pageaction.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * AttachmentPlugin for phplist.
+ * 
+ * This file is a part of AttachmentPlugin.
+ *
+ * @category  phplist
+ *
+ * @author    Duncan Cameron
+ * @copyright 2012-2017 Duncan Cameron
+ * @license   http://www.gnu.org/licenses/gpl.html GNU General Public License, Version 3
+ */
+
+/*
+ * This page and class are used to display the phplist help dialog.
+ */
+
+class AttachmentPlugin_PageactionController extends CommonPlugin_Controller
+{
+}
+
+$controller = new AttachmentPlugin_PageactionController();
+$action = isset($_GET['action']) ? $_GET['action'] : null;
+$controller->run($action);
